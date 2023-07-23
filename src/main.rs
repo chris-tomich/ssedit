1670cb7c7481bb9a38c7d556f71f9816ca18a109
Chris Tomich <chris.tomich@gmail.com>
2023-07-23 11:24:48 +1000

    Created a basic version of an array index reader

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() -> io::Result<()> {
 }
 
 fn find() {
-    let mut json_getter = JsonGet::new("batters.batter");
+    let mut json_getter = JsonGet::new("batters.batter[4]");
     let mut lines = io::stdin().lock().lines();
 
     let mut json_lexer = JsonStreamLexer::new();
@@ -110,7 +110,7 @@ enum JsonGetParseMode {
 
 enum JsonPathComponent {
     PropertyName(String),
-    ArrayIndex(usize),
+    ArrayIndex(isize),
 }
 
 struct JsonGet {
@@ -119,13 +119,15 @@ struct JsonGet {
     parse_mode: JsonGetParseMode,
     captured_tokens: Vec<JsonStreamToken>,
     current_search_depth: isize,
+    current_capture_depth: isize,
+    current_index: isize,
 }
 
 impl JsonGet {
     fn new(path: &str) -> JsonGet {
         let mut path = process_path(path);
         if let Some(starting_token) = path.pop_front() {
-            JsonGet { path: path, current_token: starting_token, parse_mode: JsonGetParseMode::Search, captured_tokens: Vec::new(), current_search_depth: 0 }
+            JsonGet { path: path, current_token: starting_token, parse_mode: JsonGetParseMode::Search, captured_tokens: Vec::new(), current_search_depth: 0, current_capture_depth: 0, current_index: -1 }
         } else {
             panic!("path isn't supported")
         }
@@ -162,48 +164,71 @@ impl JsonGet {
     fn process_token(&mut self, token: &JsonStreamToken) -> bool {
         match self.parse_mode {
             JsonGetParseMode::Search => {
-                match token.token_type {
-                    JsonTokenType::PropertyName => {
-                        if let JsonPathComponent::PropertyName(name) = &self.current_token {
-                            if token.token_parsed.eq(name) {
-                                if let Some(next_token) = self.path.pop_front() {
-                                    self.current_token = next_token;
-                                } else {
-                                    self.parse_mode = JsonGetParseMode::Capture;
+                match &self.current_token {
+                    JsonPathComponent::PropertyName(name) => {
+                        match token.token_type {
+                            JsonTokenType::PropertyName => {
+                                if token.token_parsed.eq(name) {
+                                    if let Some(next_token) = self.path.pop_front() {
+                                        self.current_token = next_token;
+                                    } else {
+                                        self.parse_mode = JsonGetParseMode::Capture;
+                                    }
                                 }
                             }
+                            _ => {}
                         }
                     }
-                    _ => {}
+                    JsonPathComponent::ArrayIndex(index) => {
+                        match token.token_type {
+                            JsonTokenType::ObjectOpen => self.current_search_depth += 1,
+                            JsonTokenType::ObjectClose => self.current_search_depth -= 1,
+                            JsonTokenType::ArrayOpen => {
+                                self.current_index = 0;
+                                self.current_search_depth = 0;
+                            }
+                            JsonTokenType::PropertyDelimiter => {
+                                if self.current_search_depth == 0 {
+                                    self.current_index += 1;
+                                }
+                            }
+                            _ => {}
+                        }
+
+                        if self.current_index == *index && self.current_search_depth == 0 {
+                            self.parse_mode = JsonGetParseMode::Capture;
+                            self.current_index = -1;
+                        }
+                    }
                 }
                 
                 false
             }
             JsonGetParseMode::Capture => {
                 match token.token_type {
-                    JsonTokenType::ObjectOpen => self.current_search_depth += 1,
+                    JsonTokenType::ObjectOpen => self.current_capture_depth += 1,
                     JsonTokenType::ObjectClose => {
-                        self.current_search_depth -= 1;
+                        self.current_capture_depth -= 1;
 
-                        if self.current_search_depth == 0 {
+                        if self.current_capture_depth == 0 {
                             self.parse_mode = JsonGetParseMode::Finish;
                         }
                     }
-                    JsonTokenType::ArrayOpen => self.current_search_depth += 1,
+                    JsonTokenType::ArrayOpen => self.current_capture_depth += 1,
                     JsonTokenType::ArrayClose => {
-                        self.current_search_depth -= 1;
+                        self.current_capture_depth -= 1;
 
-                        if self.current_search_depth == 0 {
+                        if self.current_capture_depth == 0 {
                             self.parse_mode = JsonGetParseMode::Finish;
                         }
                     }
                     JsonTokenType::KeyValueDelimiter => {
-                        if self.current_search_depth == 0 {
+                        if self.current_capture_depth == 0 {
                             return false;
                         }
                     }
                     JsonTokenType::PropertyDelimiter => {
-                        if self.current_search_depth == 0 {
+                        if self.current_capture_depth == 0 {
                             self.parse_mode = JsonGetParseMode::Finish;
                             return false;
                         }
@@ -240,13 +265,18 @@ fn process_path(path: &str) -> LinkedList<JsonPathComponent> {
                 parsed_path.push_back(JsonPathComponent::PropertyName(token_builder.clone()));
                 token_builder.clear();
             }
-            '[' => parsing_array_index = true,
+            '[' => {
+                parsed_path.push_back(JsonPathComponent::PropertyName(token_builder.clone()));
+                token_builder.clear();
+
+                parsing_array_index = true;
+            }
             ']' => {
                 if !parsing_array_index {
                     panic!("unexpected array index close");
                 }
 
-                if let Ok(array_index) = token_builder.as_str().parse::<usize>() {
+                if let Ok(array_index) = token_builder.as_str().parse::<isize>() {
                     parsed_path.push_back(JsonPathComponent::ArrayIndex(array_index));
                 } else {
                     panic!("array index couldn't be parsed to a number");
